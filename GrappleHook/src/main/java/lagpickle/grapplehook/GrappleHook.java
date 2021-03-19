bb17947588d4a2428e29c2801cd76c65d5741202
yiunkarch <57888137+yiunkarch@users.noreply.github.com>
2021-03-19 15:20:39 +0800

    Initial commit

---
+++ GrappleHook/src/main/java/lagpickle/grapplehook/GrappleHook.java
@@ -0,0 +1,11 @@
+package lagpickle.grapplehook;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GrappleHook extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
