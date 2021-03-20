8914a03940feb4934bacd7b6df486a6c3a45bc63
yiunkarch <57888137+yiunkarch@users.noreply.github.com>
2021-03-20 10:15:08 +0800

    Add configurable particle effects to tether

--- GrappleHook/src/main/java/lagpickle/grapplehook/GrappleHook.java
+++ GrappleHook/src/main/java/lagpickle/grapplehook/GrappleHook.java
@@ -1,20 +1,27 @@
 package lagpickle.grapplehook;
 
+import org.bukkit.Color;
+import org.bukkit.Particle;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GrappleHook extends JavaPlugin {
 
     private GrappleManager grappleManager;
     private GrappleListener grappleListener;
+    private GrappleHookCommand grappleHookCommand;
 
     public GrappleHook() {
         this.grappleManager = new GrappleManager(this);
         this.grappleListener = new GrappleListener(this);
+        this.grappleHookCommand = new GrappleHookCommand(this);
     }
 
     @Override
     public void onEnable() {
+        this.readConfig();
         getServer().getPluginManager().registerEvents(this.grappleListener,this);
+        getCommand("grapplehook-reload").setExecutor(this.grappleHookCommand);
     }
 
     public GrappleManager getGrappleManager() {
@@ -24,4 +31,47 @@ public class GrappleHook extends JavaPlugin {
         return this.grappleListener;
     }
 
+    private double maxTetherLength2;
+    private Particle.DustOptions particleOptions;
+    public void readConfig() {
+        saveDefaultConfig();
+        reloadConfig();
+
+        ConfigurationSection config = this.getConfig();
+
+        double maxTether = config.getDouble("max-tether-length");
+        this.maxTetherLength2 = maxTether * maxTether;
+
+        try {
+            this.particleOptions = new Particle.DustOptions(
+                Color.fromRGB(
+                    config.getInt("particle-options.red"),
+                    config.getInt("particle-options.green"),
+                    config.getInt("particle-options.blue")
+                ),
+                Double.valueOf(config.getDouble("particle-options.size")).floatValue()
+            );
+        } catch (IllegalArgumentException e) {
+            getLogger().warning("Particle options invalid, using defaults");
+            this.particleOptions = new Particle.DustOptions(Color.GRAY,1);
+        }
+    }
+    public int getHookTimeout() {
+        return this.getConfig().getInt("hook-timeout");
+    }
+    public double getRetractionSpeed() {
+        return getConfig().getDouble("retraction-speed");
+    }
+    public double getMinTetherLength() {
+        return getConfig().getDouble("min-tether-length");
+    }
+    public double getMaxTetherLength2() {
+        return this.maxTetherLength2;
+    }
+    public Particle.DustOptions getParticleOptions() {
+        return this.particleOptions;
+    }
+    public double getParticleStep() {
+        return this.getConfig().getDouble("particle-options.step");
+    }
 }
--- GrappleHook/src/main/java/lagpickle/grapplehook/LocationGrapple.java
+++ GrappleHook/src/main/java/lagpickle/grapplehook/LocationGrapple.java
@@ -1,6 +1,7 @@
 package lagpickle.grapplehook;
 
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -23,6 +24,7 @@ public class LocationGrapple extends BukkitRunnable {
 
     @Override
     public void run() {
+        // retract tether
         if (this.length > this.plugin.getMinTetherLength()) {
             this.length -= this.plugin.getRetractionSpeed();
             this.length2 = this.length * this.length;
@@ -51,6 +53,17 @@ public class LocationGrapple extends BukkitRunnable {
             );
             Vector newVelocity = newLoc.subtract(playerLoc).toVector();
             this.player.setVelocity(newVelocity);
+        }
+        // particles
+        Location loc = player.getEyeLocation().add(0,0.5,0);
+        Vector stepVec = this.hookLoc.clone().subtract(loc).toVector();
+        double stepSize = this.plugin.getParticleStep();
+        stepVec.normalize().multiply(stepSize);
+        for (double i = 0; i < this.length; i += stepSize) {
+            loc.getWorld().spawnParticle(
+                Particle.REDSTONE, loc, 1, 0,0,0,
+                this.plugin.getParticleOptions());
+            loc.add(stepVec);
         }
     }
 
